d774dc72437f7b05c041753a419667eba6511341
Dara Olayebi <dara.olayebi@yahoo.com>
2018-07-01 23:55:45 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,25 +3,28 @@
 
 // When size is submitted by the user, call makeGrid()
 
+const canvas = document.getElementById("pixelCanvas");
+const height = document.getElementById("inputHeight");
+const width = document.getElementById("inputWidth");
+const sizePicker = document.getElementById("sizePicker");
+
 // function makeGrid() {
-//     for (var row = 0; row < height.value; row++) {
-//         for (var columns = 0; columns < x; columns++) {
+//     for (let row = 0; row < height.value; row++) {
+//         for (let columns = 0; columns < x; columns++) {
 //             $("#container").append("<div class='grid'></div>");
 //         };
 //     };
 
 // }
 
-
-const canvas = document.getElementById("pixelCanvas");
-const height = document.getElementById("inputHeight");
-const width = document.getElementById("inputWidth");
-const sizePicker = document.getElementById("sizePicker");
-const colorPicker = document.getElementById("colorPicker");
-
-colorPicker.addEventListener("click", function() {
-
-});
+function makeGrid() {
+    for (let row = 0; row < height.value; row++) {
+        const row = canvas.insertRow(r);
+        for (let columns = 0; columns < width.value; columns++) {
+            const cell = row.insertCell(c);
+        }
+    }
+}
 
 sizePicker.onsubmit = function(e) {
     e.preventDefault();
@@ -29,22 +32,9 @@ sizePicker.onsubmit = function(e) {
     makeGrid();
 };
 
-function makeGrid() {
-    for (let row = 0; row < height.value; row++) {
-        const row = canvas.insertRow(r);
-        for (let column = 0; column < width.value; column++) {
-            const cell = row.insertCell(c);
-            cell.addEventListener("click", fillSquare);
-        }
-    }
-}
 
 function clearGrid() {
     while (canvas.firstChild) {
         canvas.removeChild(canvas.firstChild);
     }
-}
-
-function fillSquare() {
-    this.setAttribute("style", `background-color: ${color.value}`);
 }
